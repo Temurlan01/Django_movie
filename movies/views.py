5b78b14d46c21ec849fee5483707134b3b434027
Temurlan01 <erkinbaevtemurlan@gmail.com>
2025-04-30 16:28:42 +0600

    "сделал быструю кнопку для публикации и для снятия с публикации сделал последние добавленные и категорию сделал детальную информацию для актеров и режиссеров"

--- movies/views.py
+++ movies/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View, ListView, DetailView
 from .forms import ReviewForm
-from .models import Movie
+from .models import Movie, Category, Actor
 
 
 class MoviesView(ListView):
@@ -10,10 +10,20 @@ class MoviesView(ListView):
     queryset = Movie.objects.filter(draft=False)
     template_name = "movies/movie.html"
 
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context["categories"] = Category.objects.all()
+        return context
+
 
 class MovieDetailView(DetailView):
     model = Movie
     slug_field = "url"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context["categories"] = Category.objects.all()
+        return context
 
 
 class AddReview(View):
@@ -27,3 +37,9 @@ class AddReview(View):
             form.movie = movie
             form.save()
         return redirect(movie.get_absolute_url())
+
+
+class ActorView(DetailView):
+    model = Actor
+    template_name = 'movies/actor.html'
+    slug_field = "name"
