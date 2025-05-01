2c04b7e184215f1ea1c92f360b05ab5cd6cee0e6
Temurlan01 <erkinbaevtemurlan@gmail.com>
2025-05-01 15:37:41 +0600

    "сделал жанры и годы что бы по ним можно было находить фильмы"

--- movies/views.py
+++ movies/views.py
@@ -1,22 +1,25 @@
-
+from django.db.models import Q
 from django.shortcuts import render, redirect
 from django.views.generic import View, ListView, DetailView
 from .forms import ReviewForm
-from .models import Movie, Category, Actor
+from .models import Movie, Category, Actor, Genre
 
 
-class MoviesView(ListView):
+class GenreYear:
+    def get_genres(self):
+        return Genre.objects.all()
+
+    def get_years(self):
+        return Movie.objects.filter(draft=False).values("year")
+
+
+class MoviesView(GenreYear, ListView):
     model = Movie
     queryset = Movie.objects.filter(draft=False)
     template_name = "movies/movie.html"
 
-    def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(*args, **kwargs)
-        context["categories"] = Category.objects.all()
-        return context
 
-
-class MovieDetailView(DetailView):
+class MovieDetailView(GenreYear, DetailView):
     model = Movie
     slug_field = "url"
 
@@ -26,7 +29,7 @@ class MovieDetailView(DetailView):
         return context
 
 
-class AddReview(View):
+class AddReview( View):
     def post(self, request, pk):
         form = ReviewForm(request.POST)
         movie = Movie.objects.get(id=pk)
@@ -39,7 +42,23 @@ class AddReview(View):
         return redirect(movie.get_absolute_url())
 
 
-class ActorView(DetailView):
+class ActorView(GenreYear, DetailView):
     model = Actor
     template_name = 'movies/actor.html'
     slug_field = "name"
+
+
+class FilterMoviesView(GenreYear, ListView):
+    def get_queryset(self):
+        if 'genre' in self.request.GET and 'year' in self.request.GET:
+            print('if genre and year')
+            queryset = Movie.objects.filter(
+            Q(year__in=self.request.GET.getlist("year")) , Q(genres__in=self.request.GET.getlist("genre"))
+        )
+        else:
+            print('else')
+            queryset = Movie.objects.filter(
+                Q(year__in=self.request.GET.getlist("year")) | Q(genres__in=self.request.GET.getlist("genre"))
+            )
+        return queryset
+    template_name = "movies/movie.html"
